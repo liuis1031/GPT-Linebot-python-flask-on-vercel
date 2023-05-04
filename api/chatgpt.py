6ce2bb03d62ca50939bbe94f69c1ab82fc9eb305
liuis1031 <59944564+liuis1031@users.noreply.github.com>
2023-05-04 17:26:51 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -23,7 +23,6 @@ class ChatGPT:
             frequency_penalty=self.frequency_penalty,
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
-            use_chat_api=True
         )
         return response['choices'][0]['text'].strip()
 
