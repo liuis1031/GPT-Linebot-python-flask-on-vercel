6eb357d57fb375d37edffca8fe69d2181056066a
liuis1031 <59944564+liuis1031@users.noreply.github.com>
2023-05-05 16:15:43 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -9,14 +9,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo") #text-davinci-003
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003") #gpt-3.5-turbo
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0)) #0
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0)) #0
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6)) #0.6
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240)) #240
 
     def get_response(self):
-        response = openai.ChatCompletion.create(
+        response = openai.Completion.create(
             model=self.model,
             prompt=self.prompt.generate_prompt(),
             temperature=self.temperature,
@@ -24,7 +24,7 @@ class ChatGPT:
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        return response['choices'][0]['message']['text'].strip()
+        return response['choices'][0]['text'].strip()
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
